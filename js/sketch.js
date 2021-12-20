2704f185281da7836845770275efdd160de4d2d5
IvaniaGomes <95414233+IvaniaGomes@users.noreply.github.com>
2021-12-20 20:34:44 +0000

    update

--- js/sketch.js
+++ js/sketch.js
@@ -1,1 +1,26 @@
+function show1(){
+    x1 = document.getElementById("content1");
+    if (x1.style.display === "none"){
+        x1.style.display = "block";
+    } else {
+        x1.style.display = "none";
+    }
+}
 
+function show2(){
+    x2 = document.getElementById("content2");
+    if (x2.style.display === "none"){
+        x2.style.display = "block";
+    } else {
+        x2.style.display = "none";
+    }
+}
+
+function show3(){
+    x3 = document.getElementById("content3");
+    if (x3.style.display === "none"){
+        x3.style.display = "block";
+    } else {
+        x3.style.display = "none";
+    }
+}
